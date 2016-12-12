a7a7c5b626a13be5da2a92ff0e08f335b1df894a
Vityoube <vityoube@gmail.com>
2016-12-12 19:32:05 +0100

    Added loading texture with FreeImage library(doesn't work yet)

--- Semestr2_2stopien/MMwGC/Ex3/Ex3/Ex3.cpp
+++ Semestr2_2stopien/MMwGC/Ex3/Ex3/Ex3.cpp
@@ -1,7 +1,7 @@
 // Ex3.cpp : Defines the entry point for the console application.
 //
-
 #include "stdafx.h"
+#include "gl3.h"
 #include "GL\glut.h"
 #include "FreeImage.h"
 #include <iostream>
@@ -9,68 +9,185 @@
 double graphicLeft = 0.0, graphicRight = 100.0, graphicTop = 100.0, graphicBottom = -100.0, graphicNear = 2.0, graphicFar = 25.0;
 GLdouble sphereRadius=20.0;
 GLuint sphereSlices = 100, sphereStacks = 100;
-GLuint textureID;
-FILE* image;
 
+GLuint tex_id;
 
-FIBITMAP *loadImage(const char * filename) {
-	FIBITMAP *dib1 = NULL;
+GLuint load_texture(std::string filenameString, GLenum minificationFilter = GL_LINEAR, GLenum magnificationFilter = GL_LINEAR)
+{
+	// Get the filename as a pointer to a const char array to play nice with FreeImage
+	glMatrixMode(GL_TEXTURE);
+	const char* filename = filenameString.c_str();
 
-	FREE_IMAGE_FORMAT fif = FreeImage_GetFIFFromFilename(filename);
+	// Determine the format of the image.
+	// Note: The second paramter ('size') is currently unused, and we should use 0 for it.
+	FREE_IMAGE_FORMAT format = FreeImage_GetFileType(filename, 0);
 
-	dib1 = FreeImage_Load(fif, filename, JPEG_DEFAULT);
-	if (!dib1)
+	// Image not found? Abort! Without this section we get a 0 by 0 image with 0 bits-per-pixel but we don't abort, which
+	// you might find preferable to dumping the user back to the desktop.
+	if (format == -1)
 	{
-		std::cerr << "Error loading image" << std::endl;
-		exit(0);
+		std::cout << "Could not find image: " << filenameString.c_str() << " - Aborting." << std::endl;
+		exit(-1);
+	}
+
+	// Found image, but couldn't determine the file format? Try again...
+	if (format == FIF_UNKNOWN)
+	{
+		std::cout << "Couldn't determine file format - attempting to get from file extension..." << std::endl;
+
+		// ...by getting the filetype from the filename extension (i.e. .PNG, .GIF etc.)
+		// Note: This is slower and more error-prone that getting it from the file itself,
+		// also, we can't use the 'U' (unicode) variant of this method as that's Windows only.
+		format = FreeImage_GetFIFFromFilename(filename);
+
+		// Check that the plugin has reading capabilities for this format (if it's FIF_UNKNOWN,
+		// for example, then it won't have) - if we can't read the file, then we bail out =(
+		if (!FreeImage_FIFSupportsReading(format))
+		{
+			std::cout << "Detected image format cannot be read!" << std::endl;
+			exit(-1);
+		}
+	}
+
+	// If we're here we have a known image format, so load the image into a bitap
+	FIBITMAP* bitmap = FreeImage_Load(format, filename);
+
+	// How many bits-per-pixel is the source image?
+	int bitsPerPixel = FreeImage_GetBPP(bitmap);
+
+	// Convert our image up to 32 bits (8 bits per channel, Red/Green/Blue/Alpha) -
+	// but only if the image is not already 32 bits (i.e. 8 bits per channel).
+	// Note: ConvertTo32Bits returns a CLONE of the image data - so if we
+	// allocate this back to itself without using our bitmap32 intermediate
+	// we will LEAK the original bitmap data, and valgrind will show things like this:
+	//
+	// LEAK SUMMARY:
+	//  definitely lost: 24 bytes in 2 blocks
+	//  indirectly lost: 1,024,874 bytes in 14 blocks    <--- Ouch.
+	//
+	// Using our intermediate and cleaning up the initial bitmap data we get:
+	//
+	// LEAK SUMMARY:
+	//  definitely lost: 16 bytes in 1 blocks
+	//  indirectly lost: 176 bytes in 4 blocks
+	//
+	// All above leaks (192 bytes) are caused by XGetDefault (in /usr/lib/libX11.so.6.3.0) - we have no control over this.
+	//
+	FIBITMAP* bitmap32;
+	if (bitsPerPixel == 32)
+	{
+		std::cout << "Source image has " << bitsPerPixel << " bits per pixel. Skipping conversion." << std::endl;
+		bitmap32 = bitmap;
+	}
+	else
+	{
+		std::cout << "Source image has " << bitsPerPixel << " bits per pixel. Converting to 32-bit colour." << std::endl;
+		bitmap32 = FreeImage_ConvertTo32Bits(bitmap);
+	}
+
+	// Some basic image info - strip it out if you don't care
+	int imageWidth = FreeImage_GetWidth(bitmap32);
+	int imageHeight = FreeImage_GetHeight(bitmap32);
+	std::cout << "Image: " << filenameString.c_str() << " is size: " << imageWidth << "x" << imageHeight << "." << std::endl;
+
+	// Get a pointer to the texture data as an array of unsigned bytes.
+	// Note: At this point bitmap32 ALWAYS holds a 32-bit colour version of our image - so we get our data from that.
+	// Also, we don't need to delete or delete[] this textureData because it's not on the heap (so attempting to do
+	// so will cause a crash) - just let it go out of scope and the memory will be returned to the stack.
+	GLubyte* textureData = FreeImage_GetBits(bitmap32);
+
+	// Generate a texture ID and bind to it
+	GLuint tempTextureID;
+	glGenTextures(1, &tempTextureID);
+	glBindTexture(GL_TEXTURE_2D, tempTextureID);
+
+	// Construct the texture.
+	// Note: The 'Data format' is the format of the image data as provided by the image library. FreeImage decodes images into
+	// BGR/BGRA format, but we want to work with it in the more common RGBA format, so we specify the 'Internal format' as such.
+	glTexImage2D(GL_TEXTURE_2D,    // Type of texture
+		0,                // Mipmap level (0 being the top level i.e. full size)
+		GL_RGBA,          // Internal format
+		imageWidth,       // Width of the texture
+		imageHeight,      // Height of the texture,
+		0,                // Border in pixels
+		GL_BGRA_EXT,          // Data format
+		GL_UNSIGNED_BYTE, // Type of texture data
+		textureData);     // The image data to use for this texture
+
+						  // Specify our minification and magnification filters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minificationFilter);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magnificationFilter);
+
+	// If we're using MipMaps, then we'll generate them here.
+	// Note: The glGenerateMipmap call requires OpenGL 3.0 as a minimum.
+	if (minificationFilter == GL_LINEAR_MIPMAP_LINEAR ||
+		minificationFilter == GL_LINEAR_MIPMAP_NEAREST ||
+		minificationFilter == GL_NEAREST_MIPMAP_LINEAR ||
+		minificationFilter == GL_NEAREST_MIPMAP_NEAREST)
+	{
+		gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA, imageWidth, imageHeight,  GL_RGBA,GL_UNSIGNED_BYTE,textureData);
 
 	}
-	return dib1;
-}
 
-GLuint loadTexture(FIBITMAP * dib1) {
-	GLuint tex_id = 0;
-	int x, y;
-	int height, width;
+	// Check for OpenGL texture creation errors
+	GLenum glError = glGetError();
+	if (glError)
+	{
+		std::cout << "There was an error loading the texture: " << filenameString.c_str() << std::endl;
 
-	RGBQUAD rgbquad;
-
-
-	FREE_IMAGE_TYPE type;
-	BITMAPINFOHEADER *header;
-
-	type = FreeImage_GetImageType(dib1);
-
-
-	height = FreeImage_GetHeight(dib1);
-	width = FreeImage_GetWidth(dib1);
-
-	header = FreeImage_GetInfoHeader(dib1);
-	int scanLineWidh = ((3 * width) % 4 == 0) ? 3 * width : ((3 * width) / 4) * 4 + 4;
-	unsigned char * texels = (GLubyte*)calloc(height*scanLineWidh, sizeof(GLubyte));
-	for (x = 0; x<width; x++)
-		for (y = 0; y<height; y++)
+		switch (glError)
 		{
-			FreeImage_GetPixelColor(dib1, x, y, &rgbquad);
+		case GL_INVALID_ENUM:
+			std::cout << "Invalid enum." << std::endl;
+			break;
 
-			texels[(y*scanLineWidh + 3 * x)] = ((GLubyte*)&rgbquad)[2];
-			texels[(y*scanLineWidh + 3 * x) + 1] = ((GLubyte*)&rgbquad)[1];
-			texels[(y*scanLineWidh + 3 * x) + 2] = ((GLubyte*)&rgbquad)[0];
+		case GL_INVALID_VALUE:
+			std::cout << "Invalid value." << std::endl;
+			break;
+
+		case GL_INVALID_OPERATION:
+			std::cout << "Invalid operation." << std::endl;
+
+		default:
+			std::cout << "Unrecognised GLenum." << std::endl;
+			break;
 		}
 
-	glGenTextures(1, &tex_id);
-	glBindTexture(GL_TEXTURE_2D, tex_id);
+	}
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// Unload the 32-bit colour bitmap
+	FreeImage_Unload(bitmap32);
+
+	// If we had to do a conversion to 32-bit colour, then unload the original
+	// non-32-bit-colour version of the image data too. Otherwise, bitmap32 and
+	// bitmap point at the same data, and that data's already been free'd, so
+	// don't attempt to free it again! (or we'll crash).
+	if (bitsPerPixel != 32)
+	{
+		FreeImage_Unload(bitmap);
+	}
+
+	// Finally, return the texture ID
+	glLoadIdentity();
+	return tempTextureID;
+}
 
 
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
-		width, height, 0, GL_RGB,
-		GL_UNSIGNED_BYTE, texels);
-	free(texels);
-
-	return tex_id;
+void init() {
+	int width = 600, height = 600;
+	const float ar = (float)width / (float)height;
+	graphicLeft = -ar * 100;
+	graphicRight = ar * 100;
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glFrustum(graphicLeft, graphicRight, graphicBottom, graphicTop, graphicNear, graphicFar);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glMatrixMode(GL_TEXTURE);
+	glLoadIdentity();
+	FreeImage_Initialise(true);
 }
 void resizeScene(int width, int height) {
 	const float ar = (float)width / (float)height;
@@ -90,6 +207,8 @@ void renderScene() {
 	glTranslated(0.0, 0.0, -2.0);
 	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
 	glutSolidSphere(sphereRadius, sphereSlices, sphereStacks);
+	glMatrixMode(GL_TEXTURE);
+	glLoadIdentity();
 	glutSwapBuffers();
 }
 
@@ -101,10 +220,13 @@ int main(int argc, char** argv)
 	glutInitWindowPosition(0, 0);
 	glutInitWindowSize(640, 640);
 	glutCreateWindow("Exercise 3");
-
-	glutReshapeFunc(resizeScene);
-	glutDisplayFunc(renderScene);
-	glutMainLoop();
-    return 0;
+	init();
+	tex_id = load_texture("wood.jpg");
+	glutReshapeFunc(resizeScene);	
+	glutDisplayFunc(renderScene);	
+	glutMainLoop();	
+	FreeImage_DeInitialise();
+	return 0;
 }
+
 
