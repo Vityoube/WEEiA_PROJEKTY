ecf8c8ab6a11a55b23cf95177c426ccdfbf9a8b4
vityoube <vityoube@gmail.com>
2017-02-01 21:31:40 +0100

    Modified canvas home task

--- Semestr2_2stopien/HTML5_dla_platform_mobilnych/worker_home.js
+++ Semestr2_2stopien/HTML5_dla_platform_mobilnych/worker_home.js
@@ -6,10 +6,26 @@ function onMessage(evt){
 	var imageHeight=imageProperties[0];
 	var imageWidth=imageProperties[1];
 	console.log("Height: "+imageHeight+" Width: "+imageWidth);
-	var radius=Math.floor(Math.random()*200);
+	var radius=50;
 	var x=Math.floor(Math.random()*(imageHeight-2*radius));
-	var y=Math.floor(Math.random()*(imageWidth-2*radius));
-	while(x-radius<0 || y-radius<0 || x+radius>imageWidth || y+radius>imageHeight)
-		radius--;
+	var y;
+	while (x>radius/2 && x<imageHeight/4)
+		x--;
+	while (x<radius/2)
+		x++;
+	while (x<3*imageWidth/4 -radius/2)
+		x++;
+	while (x>imageWidth-radius/2)
+		x--;
+	if (x<imageWidth/4 || x>3*imageWidth/4)
+		y=Math.floor(Math.random()*(imageHeight));
+	else if (x>=imageWidth/4 || x<=3*imageWidth/4)
+		y=Math.floor(Math.random()*imageHeight/4);
+	while(y<radius/2)
+		y++;
+	while(y>imageHeight-radius/2)
+		y--;
+	// while(x-radius<0 || y-radius<0 || x+radius>imageWidth || y+radius>imageHeight)
+	// 	radius--;
 	postMessage([x,y,radius]);
 }
