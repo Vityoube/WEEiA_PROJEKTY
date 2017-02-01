e4ab3ebcb34cd3b93d79a5f81a38f0e3feb85590
vityoube <vityoube@gmail.com>
2017-02-01 23:07:28 +0100

    Modified exercise for webworkers in javascript

--- Semestr2_2stopien/HTML5_dla_platform_mobilnych/worker_home.js
+++ Semestr2_2stopien/HTML5_dla_platform_mobilnych/worker_home.js
@@ -1,31 +1,23 @@
 addEventListener('message',onMessage, false);
 
 function onMessage(evt){
-	var imageProperties=evt.data;
-	console.log("Received message " + imageProperties);
-	var imageHeight=imageProperties[0];
-	var imageWidth=imageProperties[1];
-	console.log("Height: "+imageHeight+" Width: "+imageWidth);
-	var radius=50;
-	var x=Math.floor(Math.random()*(imageHeight-2*radius));
-	var y;
-	while (x>radius/2 && x<imageHeight/4)
-		x--;
-	while (x<radius/2)
-		x++;
-	while (x<3*imageWidth/4 -radius/2)
-		x++;
-	while (x>imageWidth-radius/2)
-		x--;
-	if (x<imageWidth/4 || x>3*imageWidth/4)
-		y=Math.floor(Math.random()*(imageHeight));
-	else if (x>=imageWidth/4 || x<=3*imageWidth/4)
-		y=Math.floor(Math.random()*imageHeight/4);
-	while(y<radius/2)
-		y++;
-	while(y>imageHeight-radius/2)
-		y--;
-	// while(x-radius<0 || y-radius<0 || x+radius>imageWidth || y+radius>imageHeight)
-	// 	radius--;
+	// var imageProperties=evt.data;
+	// console.log("Received message " + imageProperties);
+	// var imageHeight=imageProperties[0];
+	// var imageWidth=imageProperties[1];
+	// console.log("Height: "+imageHeight+" Width: "+imageWidth);
+	var calculationProperties=evt.data;
+	var radius=calculationProperties[4];
+	var left_border_x=calculationProperties[0],right_border_x=calculationProperties[1],
+	upper_border_y=calculationProperties[2],bottom_border_y=calculationProperties[3],
+	width=calculationProperties[5],height=calculationProperties[6];
+	var x=Math.floor(Math.random()*width),y=Math.floor(Math.random()*height);
+	while (!(x>=radius && x<=left_border_x-radius && y>=radius && y<=height-radius) &&
+			!(x>=radius && x<=width-radius && y>=radius && y<=upper_border_y-radius) &&
+			!(x>=right_border_x+radius && x<=width-radius && y>=radius && y<=height-radius) &&
+			!(x>=radius && x<=width-radius && y>=bottom_border_y+radius && y<=height-radius)){
+				x=Math.floor(Math.random()*width);
+				y=Math.floor(Math.random()*height);
+			}
 	postMessage([x,y,radius]);
 }
